84fdb34d8246b1ee8d8f2c5b168cb51f8d4fe6d5
zelt <79714781+ghoulatsch@users.noreply.github.com>
2024-09-19 23:08:20 +0200

    Add files via upload

---
+++ ForceSelect.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Force Select
+// @homepage     https://superuser.com/questions/1282718/how-do-websites-block-selecting-text-and-how-do-i-unblock-that/
+// @namespace    https://superuser.com/questions/1282718/how-do-websites-block-selecting-text-and-how-do-i-unblock-that/
+// @version      1.0
+// @description  Stop sites from disabling text selection. Copy from superuser answer.
+// @author       see namespace
+// @match        *://m.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  let style = document.createElement('style');
+  style.innerHTML = '*{ user-select: auto !important; }';
+
+  document.body.appendChild(style);
+})();
