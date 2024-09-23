d18e7cd662c3d2370e3c92f390db1a50c84f40cc
zelt <79714781+ghoulatsch@users.noreply.github.com>
2024-09-23 18:53:00 +0200

    Update ForceSelect.user.js

--- ForceSelect.user.js
+++ ForceSelect.user.js
@@ -2,7 +2,7 @@
 // @name         Force Select
 // @homepage     https://github.com/ghoulatsch
 // @namespace    https://superuser.com/questions/1282718/how-do-websites-block-selecting-text-and-how-do-i-unblock-that/
-// @version      1.0
+// @version      240923
 // @description  Stop sites from disabling text selection.
 // @author       see namespace
 // @match        *://m.youtube.com/*
